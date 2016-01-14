9bdb9d4ec23930d8d67927cdce7d529badbc8b04
svenvdschootxd <svenvdschootxd@gmail.com>
2016-01-14 13:37:07 +0100

    1st commit

---
+++ ImageDownloader/Downloader.cs
@@ -0,0 +1,58 @@
+﻿using Google.Apis.Customsearch.v1;
+using Google.Apis.Customsearch.v1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageDownloader
+{
+    class Downloader
+    {
+        CustomsearchService customSearchService;
+        string searchEngineId; 
+
+        public Downloader(string apiKey, string searchEngineId)
+        {
+            customSearchService = new CustomsearchService(new Google.Apis.Services.BaseClientService.Initializer() { ApiKey = apiKey });
+            this.searchEngineId = searchEngineId;
+        }
+
+
+       
+  
+       
+
+        public void search(string query)
+        {
+
+            Google.Apis.Customsearch.v1.CseResource.ListRequest listRequest = customSearchService.Cse.List(query);
+            listRequest.Cx = searchEngineId;
+            listRequest.SearchType = Google.Apis.Customsearch.v1.CseResource.ListRequest.SearchTypeEnum.Image;
+
+            List<Result> result = new List<Result>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+
+                listRequest.Start = (i * 10) + 1;
+                Search search = listRequest.Execute();
+
+                foreach (var item in search.Items)
+                {
+                    result.Add(item);
+                }
+            }
+
+            int j = 0;
+
+            foreach (var item in result)
+            {
+                j++;
+                Console.WriteLine("Nr: " + j + " Title : " + item.Title + Environment.NewLine + "Link : " + item.Link + Environment.NewLine + Environment.NewLine);
+            }
+            
+        }
+    }
+}
---
+++ ImageDownloader/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageDownloader
+{
+    
+   
+    public partial class Form1 : Form
+    {
+
+        Downloader downloader;
+        public Form1()
+        {
+             const string apiKey = "AIzaSyC3u5jYwwZVqSq-JQge2gYHqDKErrCo1Sc";
+             const string searchEngineId = "002939223692517534743:_6gyhcdvzrg";
+
+             InitializeComponent();
+
+             downloader = new Downloader(apiKey, searchEngineId);
+        }
+
+        private void search_Click(object sender, EventArgs e)
+        {
+            downloader.search(searchBox.Text.ToString());
+            
+        }
+    }
+}
