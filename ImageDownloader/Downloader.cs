461b8a4e0e7a9633c0fb3eac8eb0b2cad5ef0d42
Raymon <Raymon.bunt@student.stenden.com>
2016-01-15 13:09:08 +0100

    Downloader complete

--- ImageDownloader/Downloader.cs
+++ ImageDownloader/Downloader.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +14,45 @@ namespace ImageDownloader
 {
     public class Downloader
     {
-        //comment
+        private int count = 0;
+        private Bitmap bitmap;
+
+        public void download(ArrayList images, string title)
+        {
+            Console.WriteLine(images.Count);
+            foreach (Image tmpImage in images)
+            {
+                try
+                {
+                    WebClient client = new WebClient();
+                    Stream stream = client.OpenRead(tmpImage.link);
+                    bitmap = new Bitmap(stream);
+                    stream.Flush();
+                    stream.Close();
+                    Console.WriteLine(tmpImage.link);
+                    SaveImage(title, bitmap.RawFormat);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public Bitmap GetImage()
+        {
+            return bitmap;
+        }
+
+        public void SaveImage(string filename, ImageFormat format)
+        {
+            count++;
+            if (bitmap != null)
+            {
+                Console.Write(format.ToString());
+                bitmap.Save(@"c:\\saves\\" + filename + count +".jpeg", ImageFormat.Jpeg);
+
+            }
+        }
     }
 }
